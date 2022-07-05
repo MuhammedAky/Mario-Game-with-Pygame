dab6b8831f548a2dbaa6976c216cc7358c62eb07
MuhammedAky <muhammedaky24@gmail.com>
2022-07-05 16:34:41 +0300

    Player Camera

--- player.py
+++ player.py
@@ -3,11 +3,11 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
+        self.speed = 8
         self.image = pygame.Surface((32,64))
         self.image.fill("red")
         self.rect = self.image.get_rect(topleft = pos)
         self.direction = pygame.math.Vector2(0,0)
-        self.speed = 8
 
     def get_input(self):
         keys = pygame.key.get_pressed()

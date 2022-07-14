ba9c43352aadef73b911f6a92444a6daed0e1ae3
MuhammedAky <muhammedaky24@gmail.com>
2022-07-14 12:57:35 +0300

    Finished the platformer

--- code/main.py
+++ code/main.py
@@ -13,22 +13,32 @@ class Game:
 		self.cur_health = 100
 		self.coins = 0
 
+		# audio
+		self.level_bg_music = pygame.mixer.Sound('../audio/level_music.wav')
+		self.overworld_bg_music = pygame.mixer.Sound('../audio/overworld_music.wav')
+
 		# overworld creation
 		self.overworld = Overworld(0,self.max_level,screen,self.create_level)
 		self.status = 'overworld'
+		self.overworld_bg_music.play(loops = -1)
 
-		# user interface 
+		# user interface
 		self.ui = UI(screen)
+
 
 	def create_level(self,current_level):
 		self.level = Level(current_level,screen,self.create_overworld,self.change_coins,self.change_health)
 		self.status = 'level'
+		self.overworld_bg_music.stop()
+		self.level_bg_music.play(loops = -1)
 
 	def create_overworld(self,current_level,new_max_level):
 		if new_max_level > self.max_level:
 			self.max_level = new_max_level
 		self.overworld = Overworld(current_level,self.max_level,screen,self.create_level)
 		self.status = 'overworld'
+		self.overworld_bg_music.play(loops = -1)
+		self.level_bg_music.stop()
 
 	def change_coins(self,amount):
 		self.coins += amount
@@ -43,6 +53,8 @@ class Game:
 			self.max_level = 0
 			self.overworld = Overworld(0,self.max_level,screen,self.create_level)
 			self.status = 'overworld'
+			self.level_bg_music.stop()
+			self.overworld_bg_music.play(loops = -1)
 
 	def run(self):
 		if self.status == 'overworld':
@@ -55,6 +67,7 @@ class Game:
 
 # Pygame setup
 pygame.init()
+pygame.display.set_caption('Mario')
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 game = Game()
@@ -64,7 +77,7 @@ while True:
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
-	
+
 	screen.fill('grey')
 	game.run()
 
